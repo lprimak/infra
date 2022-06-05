9ff2a0939a932e2fede154a7f2d28cafba2f0f5c
lprimak <lenny@flowlogix.com>
2022-06-04 19:33:40 -0500

    moved website content into generated folder

--- scripts/ssl/letsencrypt-common.sh
+++ scripts/ssl/letsencrypt-common.sh
@@ -3,7 +3,6 @@
 letsencrypt_dir=$HOME/var/letsencrypt
 
 certbot "$@" -n --webroot --agree-tos --email admin@flowlogix.com \
---webroot-path ~website/contents \
+--webroot-path $HOME/var/website-content \
 --logs-dir $HOME/var/log/letsencrypt --config-dir $letsencrypt_dir \
 --work-dir $HOME/var/lib
-
