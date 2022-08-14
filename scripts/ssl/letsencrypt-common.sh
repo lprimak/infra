1a9835b631f52b81abecb02f076c1acc875b8596
lprimak <lenny@flowlogix.com>
2022-08-14 16:08:33 -0500

    LetsEncrypt - using wildcard certificate fixes #1

--- scripts/ssl/letsencrypt-common.sh
+++ scripts/ssl/letsencrypt-common.sh
@@ -2,7 +2,8 @@
 
 letsencrypt_dir=$HOME/var/letsencrypt
 
-certbot "$@" -n --webroot --agree-tos --email admin@flowlogix.com \
---webroot-path $HOME/var/website-content \
+certbot "$@" -n --authenticator dns-dynu \
+--dns-dynu-credentials $HOME/var/dynu-credentials.ini \
+--agree-tos --email admin@flowlogix.com \
 --logs-dir $HOME/var/log/letsencrypt --config-dir $letsencrypt_dir \
 --work-dir $HOME/var/lib
