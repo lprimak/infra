a282b0a82eb99351f976206676f10cd05e6b4808
lprimak <lenny@flowlogix.com>
2022-09-24 02:27:20 -0400

    moved secrets file into a folder

--- scripts/ssl/letsencrypt-common.sh
+++ scripts/ssl/letsencrypt-common.sh
@@ -3,7 +3,7 @@
 letsencrypt_dir=$HOME/var/letsencrypt
 
 certbot "$@" -n --authenticator dns-dynu \
---dns-dynu-credentials $HOME/var/dynu-credentials.ini \
+--dns-dynu-credentials $HOME/var/secrets/dynu-credentials.ini \
 --agree-tos --email admin@flowlogix.com \
 --logs-dir $HOME/var/log/letsencrypt --config-dir $letsencrypt_dir \
 --work-dir $HOME/var/lib
