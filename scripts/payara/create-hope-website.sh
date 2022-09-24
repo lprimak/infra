2152d71c54e97536a1063b80de6036e95bc2682d
lprimak <lenny@flowlogix.com>
2022-09-24 02:59:21 -0400

    added password aliases for database access to prod domain

--- scripts/payara/create-hope-website.sh
+++ scripts/payara/create-hope-website.sh
@@ -8,6 +8,8 @@ domain_dir=$HOME/var/payara-domains/hope-website/config
 keytool -importkeystore -srckeystore $domain_dir/cacerts.jks -destkeystore $domain_dir/cacerts.jks -deststoretype pkcs12 -srcstorepass changeit -deststorepass changeit 2>/dev/null
 $SCRIPT_DIR/import-certs.sh
 
+asadmin create-password-alias hope-db-username --passwordfile $HOME/var/secrets/hope-db-usernamefile
+asadmin create-password-alias hope-db-password --passwordfile $HOME/var/secrets/hope-db-passwordfile
+
 asadmin deploy --contextroot mail $HOME/apps/cubusmail/bin/cubusmail.war
 asadmin restart-domain
-
