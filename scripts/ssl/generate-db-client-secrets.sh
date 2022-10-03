ef22669d028a217a67e7c86f31266111e9576141
lprimak <lenny@flowlogix.com>
2022-10-02 22:11:37 -0500

    - added scripts for generating database secrets - ejb timers use data grid instead of a database

---
+++ scripts/ssl/generate-db-client-secrets.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh -p
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <username>" >&2
+  exit 1
+fi
+client_dir=client/$1
+mkdir -p client
+mkdir $client_dir
+[ $? != 0 ] && exit 1
+ln -s ../../server/root.crt $client_dir/
+openssl ecparam -name prime256v1 -genkey -noout -out $client_dir/postgresql.key
+openssl pkcs8 -topk8 -inform PEM -outform DER -nocrypt -in $client_dir/postgresql.key -out $client_dir/postgresql.pk8
+openssl req -new -sha256 -key $client_dir/postgresql.key -out $client_dir/client.csr -subj "/CN=$1"
+
+openssl x509 -req -in $client_dir/client.csr -CA $client_dir/root.crt -CAkey server/ca.key -CAcreateserial -out $client_dir/postgresql.crt -days 365 -sha256
+
+chmod og-rwx,g+rw $client_dir/*.key $client_dir/postgresql.crt $client_dir/*.pk8
+rm -f $client_dir/*.csr $client_dir/*.srl
---
+++ scripts/ssl/generate-db-server-secrets.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh -p
+
+mkdir server
+[ $? != 0 ] && exit 1
+openssl ecparam -name prime256v1 -genkey -noout -out server/ca.key
+openssl req -new -x509 -sha256 -key server/ca.key -out server/root.crt -subj "/CN=ca.flowlogix.com"
+
+openssl ecparam -name prime256v1 -genkey -noout -out server/server.key
+openssl req -new -sha256 -key server/server.key -out server/server.csr -subj "/CN=db.hope.nyc.ny.us"
+
+openssl x509 -req -in server/server.csr -CA server/root.crt -CAkey server/ca.key -CAcreateserial -out server/server.crt -days 365 -sha256
+
+chmod og-rwx server/*.key
