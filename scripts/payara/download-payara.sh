a4f4cbca08cf37e92e79abe887840d291bdd2b54
lprimak <lenny@flowlogix.com>
2022-10-03 14:34:05 -0500

    download-payara script works on non-prod machine as well

--- scripts/payara/download-payara.sh
+++ scripts/payara/download-payara.sh
@@ -5,8 +5,12 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 payara_version=$1
-temp_dir=${HOME}/apps/payara/temp-${payara_version}
-target_dir=${HOME}/apps/payara/payara-${payara_version}
+payara_basedir=${HOME}/apps/payara
+if [ ! -d ${HOME}/apps/payara ]; then
+    payara_basedir=$PWD/payara
+fi
+temp_dir=$payara_basedir/temp-${payara_version}
+target_dir=$payara_basedir/payara-${payara_version}
 
 if [ -d $target_dir ]; then
     echo "$target_dir already exists"
@@ -19,13 +23,16 @@ mvn dependency:unpack -Dartifact=fish.payara.distributions:payara:${payara_versi
 mv $temp_dir/payara5 $target_dir
 rm -rf $temp_dir
 
-cat << EOF >> $target_dir/glassfish/config/asenv.conf
+if [ -d ${HOME}/apps/payara ]; then
+    cat << EOF >> $target_dir/glassfish/config/asenv.conf
 
-# Override Defaults
-export SDKMAN_DIR="${HOME}/.sdkman"
-[[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
-# sdk use java 11.0.14-zulu
-AS_DEF_DOMAINS_PATH="\${HOME}/var/payara-domains"
-AS_DEF_NODES_PATH="\${HOME}/var/payara-nodes"
-export AS_ADMIN_PORT=1148
+    # Override Defaults
+    export SDKMAN_DIR="${HOME}/.sdkman"
+    [[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
+    # sdk use java 11.0.14-zulu
+    AS_DEF_DOMAINS_PATH="\${HOME}/var/payara-domains"
+    AS_DEF_NODES_PATH="\${HOME}/var/payara-nodes"
+    export AS_ADMIN_PORT=1148
 EOF
+
+fi
