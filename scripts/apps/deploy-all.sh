4da01d3ee8dd385912a0857659246424b3677a9e
lprimak <lenny@flowlogix.com>
2022-11-13 00:57:54 -0700

    fixed apps download script

--- scripts/apps/deploy-all.sh
+++ scripts/apps/deploy-all.sh
@@ -2,8 +2,7 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-$SCRIPT_DIR/download-all.sh
+$SCRIPT_DIR/download-all-apps.sh
 
 asadmin deploy --force --contextroot jee-examples --availabilityenabled $HOME/apps/hope-apps/jee-examples.war
 asadmin deploy --force --contextroot em --availabilityenabled $HOME/apps/hope-apps/hope-website.war
-
