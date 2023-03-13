1081567a69e061435f960b83444b9a429995495c
lprimak <lenny@flowlogix.com>
2023-03-13 15:42:27 -0400

    update to flowlogix 6

--- scripts/apps/download-all-apps.sh
+++ scripts/apps/download-all-apps.sh
@@ -2,8 +2,5 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-$SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-ee-integration/job/main/lastSuccessfulBuild/artifact/com/flowlogix/jee-examples/5.x-SNAPSHOT/jee-examples-5.x-SNAPSHOT.war $HOME/apps/hope-apps/jee-examples.war
-$SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-org-repo/job/jee10-examples/job/main/lastSuccessfulBuild/artifact/com/flowlogix/examples/jee10-examples/1.x-SNAPSHOT/jee10-examples-1.x-SNAPSHOT.war $HOME/apps/hope-apps/jee-examples-jee10.war
-
+$SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-ee-integration/job/main/lastSuccessfulBuild/artifact/com/flowlogix/jee-examples/6.x-SNAPSHOT/jee-examples-6.x-SNAPSHOT.war $HOME/apps/hope-apps/jee-examples.war
 $SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-org-repo/job/apps/job/main/lastSuccessfulBuild/artifact/com/flowlogix/hope-website/0.0.1-SNAPSHOT/hope-website-0.0.1-SNAPSHOT.war $HOME/apps/hope-apps/hope-website.war
-$SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-org-repo/job/apps/job/jakarta-ee-10/lastSuccessfulBuild/artifact/com/flowlogix/hope-website/0.0.1-SNAPSHOT/hope-website-0.0.1-SNAPSHOT.war $HOME/apps/hope-apps/hope-website-jee10.war
