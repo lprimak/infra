ce40414f45421d5b86a550e6ed6f3b6b3a56d5c4
lprimak <lenny@flowlogix.com>
2023-05-27 17:51:14 -0500

    update: extracted hope examples version

--- scripts/apps/download-all-apps.sh
+++ scripts/apps/download-all-apps.sh
@@ -2,5 +2,7 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-$SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-ee-integration/job/main/lastSuccessfulBuild/artifact/com/flowlogix/jee-examples/6.x-SNAPSHOT/jee-examples-6.x-SNAPSHOT.war $HOME/apps/hope-apps/jee-examples.war
+EXAMPLES_VERSION=7.x-SNAPSHOT
+
+$SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-ee-integration/job/main/lastSuccessfulBuild/artifact/com/flowlogix/jee-examples/$EXAMPLES_VERSION/jee-examples-${EXAMPLES_VERSION}.war $HOME/apps/hope-apps/jee-examples.war
 $SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-org-repo/job/apps/job/main/lastSuccessfulBuild/artifact/com/flowlogix/hope-website/1.x-SNAPSHOT/hope-website-1.x-SNAPSHOT.war $HOME/apps/hope-apps/hope-website.war
