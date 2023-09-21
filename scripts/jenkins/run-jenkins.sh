e828ca345bbaca7b26b7a7ca9df9302c1e69256e
lprimak <lenny@flowlogix.com>
2023-09-20 19:54:26 -0500

    enh: enabled ZGC / Generational ZGC for Jenkins

--- scripts/jenkins/run-jenkins.sh
+++ scripts/jenkins/run-jenkins.sh
@@ -8,7 +8,7 @@ unset AS_ADMIN_PASSWORDFILE
 
 jenkins_bin=$HOME/apps/jenkins/current
 
-exec java -Xmx1g \
+exec java -Xmx1g -XX:+UseZGC -XX:+ZGenerational \
 --add-opens java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED \
 --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED \
 --add-opens java.base/sun.util.calendar=ALL-UNNAMED \
