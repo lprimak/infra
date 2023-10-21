0cc3e8e1c914eefa9d927fabd6f283f6cc5af016
lprimak <lenny@flowlogix.com>
2023-10-21 01:27:55 -0500

    propagate rewnewal cert to web server

--- scripts/ssl/letsencrypt-after-renewal.sh
+++ scripts/ssl/letsencrypt-after-renewal.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=`dirname "$0"`
 
 $SCRIPT_DIR/../payara/import-certs.sh 2>/dev/null
+OCI_DIR=$HOME/infra/scripts/cloud/oci
 
 asadmin -I=false multimode << EOF
 set configs.config.server-config.network-config.network-listeners.network-listener.http-listener-2.enabled=false
@@ -21,6 +22,8 @@ echo "commit ssl cert $HOME/var/ssl-links/hope-fullchain.pem" | socat tcp-connec
 echo -e "set ssl cert $HOME/var/ssl-links/lp-fullchain.pem <<\n$(cat $HOME/var/ssl-links/lp-fullchain.pem*)\n" | \
 socat tcp-connect:localhost:9999 -
 echo "commit ssl cert $HOME/var/ssl-links/lp-fullchain.pem" | socat tcp-connect:localhost:9999 -
+
+ansible-playbook -i $OCI_DIR/hosts $OCI_DIR/install-software.yml
 
 # Email Notification
 # Original script located at https://github.com/hstock/certbot-renew-email
