702fd7e78b5bfb6715d8e6d31c9e864f81f0e6c9
lprimak <lenny@flowlogix.com>
2023-11-10 02:17:47 -0600

    enh: split up ssl links

--- scripts/ssl/letsencrypt-initial.sh
+++ scripts/ssl/letsencrypt-initial.sh
@@ -2,9 +2,6 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-cert_links_src=$HOME/var/letsencrypt/live
-cert_links_dst=$HOME/var/ssl-links
-
 $SCRIPT_DIR/letsencrypt-common.sh certonly -d '*.hope.nyc.ny.us,hope.nyc.ny.us' \
 --authenticator dns-dynu --dns-dynu-credentials $HOME/var/secrets/dynu-credentials.ini \
 "$@"
@@ -15,11 +12,4 @@ $SCRIPT_DIR/letsencrypt-common.sh certonly -d '*.flowlogix.com,flowlogix.com' \
 $SCRIPT_DIR/letsencrypt-common.sh certonly -d 'lennyprimak.com,www.lennyprimak.com' \
 --webroot -w $HOME/var/website-content "$@"
 
-rm -rf $cert_links_dst
-mkdir -p $cert_links_dst
-ln -s $cert_links_src/hope.nyc.ny.us/fullchain.pem $cert_links_dst/hope-fullchain.pem
-ln -s $cert_links_src/hope.nyc.ny.us/privkey.pem $cert_links_dst/hope-fullchain.pem.key
-ln -s $cert_links_src/lennyprimak.com/fullchain.pem $cert_links_dst/lp-fullchain.pem
-ln -s $cert_links_src/lennyprimak.com/privkey.pem $cert_links_dst/lp-fullchain.pem.key
-ln -s $cert_links_src/flowlogix.com/fullchain.pem $cert_links_dst/fl-fullchain.pem
-ln -s $cert_links_src/flowlogix.com/privkey.pem $cert_links_dst/fl-fullchain.pem.key
+$SCRIPT_DIR/ssl-links.sh
---
+++ scripts/ssl/ssl-links.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+cert_links_src=$HOME/var/letsencrypt/live
+cert_links_dst=$HOME/var/ssl-links
+
+
+rm -rf $cert_links_dst
+mkdir -p $cert_links_dst/docker
+ln -s $cert_links_src/hope.nyc.ny.us/fullchain.pem $cert_links_dst/hope-fullchain.pem
+ln -s $cert_links_src/hope.nyc.ny.us/privkey.pem $cert_links_dst/hope-fullchain.pem.key
+ln -s $cert_links_src/lennyprimak.com/fullchain.pem $cert_links_dst/lp-fullchain.pem
+ln -s $cert_links_src/lennyprimak.com/privkey.pem $cert_links_dst/lp-fullchain.pem.key
+ln -s $cert_links_src/flowlogix.com/fullchain.pem $cert_links_dst/fl-fullchain.pem
+ln -s $cert_links_src/flowlogix.com/privkey.pem $cert_links_dst/fl-fullchain.pem.key
+
+ln -s $cert_links_src/flowlogix.com/fullchain.pem $cert_links_dst/docker/cert.pem
+ln -s $cert_links_dst/docker/cert.pem $cert_links_dst/docker/ca.pem
+ln -s $cert_links_src/flowlogix.com/privkey.pem $cert_links_dst/docker/key.pem
