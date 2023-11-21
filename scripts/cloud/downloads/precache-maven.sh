d5f5deb3a89ca9cfd052ed0af156aff1b5e8312d
lprimak <lenny@flowlogix.com>
2023-11-20 23:30:18 -0600

    jenkins: added docker pipeline plugin

--- scripts/cloud/downloads/precache-maven.sh
+++ scripts/cloud/downloads/precache-maven.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/flowlogix/flowlogix
 mvn -f flowlogix -Dwebdriver.gecko.driver=/usr/bin/geckodriver \
 	-Dwebdriver.browser=firefox \
-	-Pui-test verify -DskipTests -DskipITs
+	-Pui-test,coverage verify -DskipTests -DskipITs -Djacoco.skip
--- scripts/jenkins/run-agent.sh
+++ scripts/jenkins/run-agent.sh
@@ -1,22 +1,31 @@
-#!/bin/zsh -l
+#!/bin/bash -l
 
-source ~/.zshrc
+source ~/.bashrc
 unset AS_ADMIN_USER
 unset AS_ADMIN_PASSWORDFILE
 
+mkdir -p $HOME/apps/jenkins/current $HOME/var/jenkins
 agent_path=$HOME/apps/jenkins/current/agent.jar
 cd $HOME/var/jenkins
 
-week_ago=$(gdate -d 'now - 7 days' +%s)
-agent_time=$(gdate -r "$agent_path" +%s) 2>/dev/null
+platform=$(uname)
+date_cmd=date
+if [ "$platform" == "Darwin" ]; then
+  date_cmd=gdate
+fi
+week_ago=$($date_cmd -d 'now - 7 days' +%s)
+if [ -e "$agent_path" ]; then
+  agent_time=$($date_cmd -r "$agent_path" +%s)
+fi
 
 if [ ! -e "$agent_path" ] || (( agent_time <= week_ago )); then
+    echo "Downloading Agent ..."
     curl -s https://jenkins.hope.nyc.ny.us/jnlpJars/agent.jar -o "$agent_path"
 fi
 
 while true; do
     java -jar "$agent_path" \
-        -jnlpUrl https://jenkins.hope.nyc.ny.us/computer/nova/jenkins-agent.jnlp \
+        -jnlpUrl https://jenkins.hope.nyc.ny.us/computer/$1/jenkins-agent.jnlp \
         -secret @$HOME/var/secrets/jenkins-agent -workDir "$HOME/var/jenkins/hope-node"
     sleep 1
 done
