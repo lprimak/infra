5b245fe501cbd8a43d39744cd08cda54c528291c
lprimak <lenny@flowlogix.com>
2023-11-29 10:29:49 -0500

    extracted pk8 key

--- scripts/postgres/generate-db-client-secrets.sh
+++ scripts/postgres/generate-db-client-secrets.sh
@@ -10,10 +10,11 @@ mkdir $client_dir
 [ $? != 0 ] && exit 1
 ln -s ../../server/root.crt $client_dir/
 openssl ecparam -name prime256v1 -genkey -noout -out $client_dir/postgresql.key
-openssl pkcs8 -topk8 -inform PEM -outform DER -nocrypt -in $client_dir/postgresql.key -out $client_dir/postgresql.pk8
 openssl req -new -sha256 -key $client_dir/postgresql.key -out $client_dir/client.csr -subj "/CN=$1"
 
 openssl x509 -req -in $client_dir/client.csr -CA $client_dir/root.crt -CAkey server/ca.key -CAcreateserial -out $client_dir/postgresql.crt -days 365 -sha256
 
 chmod og-rwx,g+rw $client_dir/*.key $client_dir/postgresql.crt $client_dir/*.pk8
-rm -f $client_dir/*.csr $client_dir/*.srl
+rm -f $client_dir/*.csr $client_dir/*.srl $client_dir/root.crt
+
+echo generate pk8 key: openssl pkcs8 -topk8 -inform PEM -outform DER -nocrypt -in postgresql.key -out postgresql.pk8
