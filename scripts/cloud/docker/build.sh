dab449ca4852cd7b5bc96814d6f30ee6014d9ad8
lprimak <lenny@flowlogix.com>
2023-12-13 22:19:36 -0500

    deps: update maven to 4-alpha-9 enh: add payara docker image

--- scripts/cloud/docker/build.sh
+++ scripts/cloud/docker/build.sh
@@ -26,6 +26,8 @@ docker rm $container > /dev/null
 docker rmi maven-builder > /dev/null
 docker rmi payara-builder > /dev/null
 
-cp $SCRIPT_DIR/agent/*.build $SCRIPT_DIR/context/
+cp $SCRIPT_DIR/common/*.build $SCRIPT_DIR/context/
 docker buildx build --platform linux/arm64,linux/amd64 context \
 -t lprimak/jenkins-agent-maven4 -f agent/Dockerfile --push
+docker buildx build --platform linux/arm64,linux/amd64 context \
+-t lprimak/payara-full -f payara/Dockerfile --push
