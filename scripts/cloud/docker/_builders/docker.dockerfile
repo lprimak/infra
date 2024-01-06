b3fc4f356a679888d122c684b5bdf6b2ef92606c
lprimak <lenny@flowlogix.com>
2024-01-05 23:45:05 -0500

    enh: forwarding docker login to container builder

--- scripts/cloud/docker/_builders/docker.dockerfile
+++ scripts/cloud/docker/_builders/docker.dockerfile
@@ -3,6 +3,8 @@ FROM ubuntu
 ENV FL_IS_BUILD_IMAGE=yes
 COPY install-docker.sh /root
 RUN ln -s /root/infra/scripts/cloud/docker /build \
-    && /root/install-docker.sh && mkdir -p /root/.ssh
+    && /root/install-docker.sh \
+    && mkdir -p /root/.ssh /root/.docker \
+    && chmod 700 /root/.docker
 
 CMD ["bash", "-l"]
--- scripts/cloud/docker/images-builder.sh
+++ scripts/cloud/docker/images-builder.sh
@@ -2,10 +2,7 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-. $SCRIPT_DIR/common/functions.sh
-
-setup
-docker build -t docker-builder -f $SCRIPT_DIR/_builders/docker.dockerfile $SCRIPT_DIR/context
+docker build -t docker-builder -f $SCRIPT_DIR/_builders/docker.dockerfile $SCRIPT_DIR/_builders
 container=$(docker create -it --rm \
     -v /var/run/docker.sock:/var/run/docker.sock docker-builder)
 docker start $container
