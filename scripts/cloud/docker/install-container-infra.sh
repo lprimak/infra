d6c5d74720007ec3a0f0f81ed6481f67577fd525
lprimak <lenny@flowlogix.com>
2024-01-06 00:10:07 -0500

    enh(docker): don't print extra error messages

--- scripts/cloud/docker/install-container-infra.sh
+++ scripts/cloud/docker/install-container-infra.sh
@@ -9,6 +9,6 @@ echo \"Host github.com
     IdentityFile ~/.ssh/github-key.pem
     StrictHostKeyChecking = accept-new\" > $home_dir/.ssh/config"
 docker cp -qa ~/.ssh/github-key.pem $container:$home_dir/.ssh
-docker cp -qa ~/var/secrets/docker-config.json $container:$home_dir/.docker/config.json
-docker exec $container git clone git@github.com:lprimak/infra $home_dir/infra 2> /dev/null
+docker cp -qa ~/var/secrets/docker-config.json $container:$home_dir/.docker/config.json 2>/dev/null
+docker exec $container git clone git@github.com:lprimak/infra $home_dir/infra 2>/dev/null
 docker cp -qa $SCRIPT_DIR/../../ $container:$home_dir/infra/
