c7bbfb53b16d3a2466d370c94ec33e883482ab46
lprimak <lenny@flowlogix.com>
2024-01-06 12:00:45 -0500

    enh(docker): refactored builders into functions

--- scripts/cloud/docker/build.sh
+++ scripts/cloud/docker/build.sh
@@ -4,35 +4,18 @@ SCRIPT_DIR=`dirname "$0"`
 
 [ -z "${FL_IS_BUILD_IMAGE}" ] && echo "Must be running in a docker container" && exit 1
 
-. $SCRIPT_DIR/versions
 . $SCRIPT_DIR/common/functions.sh
 
 setup
 
 docker login
-echo "Creating Maven Builders"
-docker build -t maven-3-builder $SCRIPT_DIR/context --build-arg MAVEN_MAJOR_VERSION=3 \
-    --build-arg MAVEN_VERSION=$MAVEN_3_VERSION -f $SCRIPT_DIR/_builders/maven.dockerfile
-docker build -t maven-4-builder $SCRIPT_DIR/context --build-arg MAVEN_MAJOR_VERSION=4 \
-    --build-arg MAVEN_VERSION=$MAVEN_4_VERSION -f $SCRIPT_DIR/_builders/maven.dockerfile
-
-echo "Creating Payara Builders"
-docker build -t payara-5-builder --build-arg PAYARA_VERSION=$PAYARA_5_VERSION \
-    $SCRIPT_DIR/context -f $SCRIPT_DIR/_builders/payara.dockerfile
-docker build -t payara-6-builder --build-arg PAYARA_VERSION=$PAYARA_6_VERSION \
-    $SCRIPT_DIR/context -f $SCRIPT_DIR/_builders/payara.dockerfile
-docker build -t payara-default-domain $SCRIPT_DIR/context -f $SCRIPT_DIR/_builders/payara-domain.dockerfile
-
-echo "Caching maven reporitory"
+create_maven_builders
+create_payara_builders
+echo "Caching maven repository"
 $SCRIPT_DIR/_builders/cache.sh maven-4-builder
 
-echo "Copying built products to $exports_dir"
-copy_export payara-6-builder payara payara-6
-copy_export payara-5-builder payara payara-5
-copy_export payara-default-domain default-domain default-domain
-copy_export maven-3-builder maven maven-3
-copy_export maven-4-builder maven maven-4
-echo "Finished copying built products"
+export_maven_from_builders
+export_payara_from_builders
 
 docker_build lprimak/jenkins-agent-maven4 agent.dockerfile 4 6
 docker_build lprimak/jenkins-master agent-master.dockerfile 4 6
--- scripts/cloud/docker/common/functions.sh
+++ scripts/cloud/docker/common/functions.sh
@@ -1,3 +1,5 @@
+. $SCRIPT_DIR/versions
+
 function setup() {
     exports_dir=$SCRIPT_DIR/context/exports
     rm -rf $SCRIPT_DIR/context
@@ -16,11 +18,42 @@ function setup() {
     cp $SCRIPT_DIR/common/*.dockerfile $SCRIPT_DIR/context/
 }
 
+function create_maven_builders() {
+    echo "Creating Maven Builders"
+    docker build -t maven-3-builder $SCRIPT_DIR/context --build-arg MAVEN_MAJOR_VERSION=3 \
+        --build-arg MAVEN_VERSION=$MAVEN_3_VERSION -f $SCRIPT_DIR/_builders/maven.dockerfile
+    docker build -t maven-4-builder $SCRIPT_DIR/context --build-arg MAVEN_MAJOR_VERSION=4 \
+        --build-arg MAVEN_VERSION=$MAVEN_4_VERSION -f $SCRIPT_DIR/_builders/maven.dockerfile
+}
+
+function create_payara_builders() {
+    echo "Creating Payara Builders"
+    docker build -t payara-5-builder --build-arg PAYARA_VERSION=$PAYARA_5_VERSION \
+        $SCRIPT_DIR/context -f $SCRIPT_DIR/_builders/payara.dockerfile
+    docker build -t payara-6-builder --build-arg PAYARA_VERSION=$PAYARA_6_VERSION \
+        $SCRIPT_DIR/context -f $SCRIPT_DIR/_builders/payara.dockerfile
+    docker build -t payara-default-domain $SCRIPT_DIR/context -f $SCRIPT_DIR/_builders/payara-domain.dockerfile
+}
+
 function copy_export() {
     local container=$(docker create $1)
     docker cp -q $container:/var/build/$2.tar.gz $exports_dir/$3.tar.gz
     docker rm $container
     docker rmi $1 > /dev/null
+}
+
+function export_maven_from_builders() {
+    echo "Copying maven built products to $exports_dir"
+    copy_export maven-3-builder maven maven-3
+    copy_export maven-4-builder maven maven-4
+}
+
+function export_payara_from_builders() {
+    echo "Copying payara built products to $exports_dir"
+    copy_export payara-6-builder payara payara-6
+    copy_export payara-5-builder payara payara-5
+    copy_export payara-default-domain default-domain default-domain
+    echo "Finished copying built products"
 }
 
 function docker_build() {
