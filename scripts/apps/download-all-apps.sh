de2a7e983a262e683d29f99b691f11e67aaff248
lprimak <lenny@flowlogix.com>
2024-03-07 20:44:35 -0600

    deps: FL 9.x

--- scripts/apps/download-all-apps.sh
+++ scripts/apps/download-all-apps.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-EXAMPLES_VERSION=8.x-SNAPSHOT
+EXAMPLES_VERSION=9.x-SNAPSHOT
 
 $SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-ee-integration/job/main/lastSuccessfulBuild/artifact/com/flowlogix/jee-examples/$EXAMPLES_VERSION/jee-examples-${EXAMPLES_VERSION}.war $HOME/apps/hope-apps/jee-examples.war
 $SCRIPT_DIR/download-app.sh https://jenkins.hope.nyc.ny.us/job/flowlogix-org-repo/job/apps/job/main/lastSuccessfulBuild/artifact/com/flowlogix/hope-website/1.x-SNAPSHOT/hope-website-1.x-SNAPSHOT.war $HOME/apps/hope-apps/hope-website.war
