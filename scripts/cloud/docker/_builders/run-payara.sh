f92825b20fade0fe18e35abecd7e03c30b8a2cfb
lprimak <lenny@flowlogix.com>
2024-05-13 21:31:46 -0500

    bugfix(docker): fixed run-payara script to run all JDKs and Payara versions

--- scripts/cloud/docker/_builders/run-payara.sh
+++ scripts/cloud/docker/_builders/run-payara.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Use only one JVM to run Payara
 exec $(asadmin start-domain --debug -v --dry-run $PAYARA_ARGS \
-| fgrep -v "Dump of JVM Invocation line" \
-| sed -n -e '2,/^$/p') < /dev/null
+| fgrep -v -e "Dump of JVM Invocation line" \
+-e "Unable to create a system terminal" \
+-e "JVM invocation command line") < /dev/null
