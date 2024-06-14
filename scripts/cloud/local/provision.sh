fd49aa49b905a6d1e8c7e85655a653dfd458b71f
lprimak <lenny@flowlogix.com>
2024-06-13 21:18:03 -0500

    enh(k8s): setting timezone for all containers

--- scripts/cloud/local/provision.sh
+++ scripts/cloud/local/provision.sh
@@ -8,5 +8,6 @@ do
   eval $(route -n | awk -v interface=$interface '{ if ($8 ==interface && $1 == "0.0.0.0" && $2 != "0.0.0.0") print "route del default gw " $2; }')
 done
 route add default gw 10.0.1.1
+printf "search hope.nyc.ny.us\nnameserver 10.0.1.210\n" > /etc/resolv.conf
 
 exec sed -i "s/.*$hostname.*/$ip_addr $hostname/" /etc/hosts
