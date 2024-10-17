e17d65f53a817b4fd2cfa62f599430d040fea416
lprimak <lenny@flowlogix.com>
2024-10-17 13:53:16 -0500

    deps: update jdk minor versions

--- scripts/nexus/start-from-launchd.sh
+++ scripts/nexus/start-from-launchd.sh
@@ -3,6 +3,6 @@
 source ~/.zshrc
 
 ulimit -n 131072
-sdk use java 17.0.12-zulu
+sdk use java 17.0.13-zulu
 export NEXUS_SECRETS_KEY_FILE=~/var/secrets/sonatype-nexus-keys
 exec $HOME/apps/nexus/current/bin/nexus run
