6ed28bfa91de978768b49828fc530ad38f6ba2d4
lprimak <lenny@flowlogix.com>
2025-03-09 15:26:31 -0400

    chore(nexus): update download filename for nexus convention

--- scripts/nexus/download-nexus.sh
+++ scripts/nexus/download-nexus.sh
@@ -6,13 +6,16 @@ if [ $# -eq 0 ]; then
 fi
 nexus_version=$1
 target_dir=${HOME}/apps/nexus/nexus-${nexus_version}
+if [ ! -d ${HOME}/apps/nexus ]; then
+    target_dir=$PWD/nexus-${nexus_version}
+fi
 
 if [ -d $target_dir -o -f ${target_dir}.gz ]; then
     echo "$target_dir or ${target_dir}.gz already exists"
     exit 1
 fi
 
-wget https://download.sonatype.com/nexus/3/nexus-${nexus_version}-unix.tar.gz -q -O ${target_dir}.gz
+wget https://download.sonatype.com/nexus/3/nexus-mac-aarch64-${nexus_version}.tar.gz -q -O ${target_dir}.gz
 if [ $? -ne 0 ]; then
     rm -f ${target_dir}.gz
     echo "Unable to download nexus"
@@ -23,7 +26,6 @@ mkdir -p $target_dir
 rm -rf $target_dir/../sonatype-work
 gunzip -c ${target_dir}.gz | tar xf - -C $target_dir/..
 rm -f ${target_dir}.gz
-
 rm -rf $target_dir/../sonatype-work
 
 sed -e 's+-Xms.*+-Xms500m+g' \
