c0858a03d6f2529ee2b604931851a0a64e7e9182
lprimak <lenny@flowlogix.com>
2025-03-09 22:20:13 -0400

    chore(nexus): refactored nexus download script

--- scripts/nexus/download-nexus.sh
+++ scripts/nexus/download-nexus.sh
@@ -6,32 +6,43 @@ if [ $# -eq 0 ]; then
 fi
 nexus_version=$1
 target_dir=${HOME}/apps/nexus/nexus-${nexus_version}
+
 if [ ! -d ${HOME}/apps/nexus ]; then
     target_dir=$PWD/nexus-${nexus_version}
 fi
+tmp_dir=${target_dir}.tmp
+nexus_tmp_dir=${tmp_dir}/nexus-${nexus_version}
 
-if [ -d $target_dir -o -f ${target_dir}.gz ]; then
-    echo "$target_dir or ${target_dir}.gz already exists"
+if [ -d $tmp_dir ]; then
+    rm -rf $tmp_dir
+fi
+
+if [ -d $target_dir ]; then
+    echo "$target_dir already exists"
     exit 1
 fi
 
-wget https://download.sonatype.com/nexus/3/nexus-mac-aarch64-${nexus_version}.tar.gz -q -O ${target_dir}.gz
+mkdir -p ${nexus_tmp_dir}
+wget https://download.sonatype.com/nexus/3/nexus-mac-aarch64-${nexus_version}.tar.gz -q \
+-O ${nexus_tmp_dir}.gz
+
 if [ $? -ne 0 ]; then
-    rm -f ${target_dir}.gz
+    rm -rf ${tmp_dir}
+    rm -rf ${target_dir}
     echo "Unable to download nexus"
     exit 1
 fi
 
-mkdir -p $target_dir
-rm -rf $target_dir/../sonatype-work
-gunzip -c ${target_dir}.gz | tar xf - -C $target_dir/..
-rm -f ${target_dir}.gz
-rm -rf $target_dir/../sonatype-work
+gunzip -c ${nexus_tmp_dir}.gz | tar xf - -C $tmp_dir
+# ln -s ${HOME}/var/sonatype-work $target_dir/../sonatype-work
 
 sed -e 's+-Xms.*+-Xms500m+g' \
 -e 's+-Xmx.*+-Xmx2703m+g' \
 -e '/-Xms.*/i\
 -XX:-MaxFDLimit' \
 -e $(eval echo 's+../sonatype-work+${HOME}/var/sonatype-work+g') \
-${target_dir}/bin/nexus.vmoptions > ${target_dir}/bin/nexus.vmoptions.2
-mv ${target_dir}/bin/nexus.vmoptions.2 ${target_dir}/bin/nexus.vmoptions
+${nexus_tmp_dir}/bin/nexus.vmoptions > ${nexus_tmp_dir}/bin/nexus.vmoptions.2
+mv ${nexus_tmp_dir}/bin/nexus.vmoptions.2 ${nexus_tmp_dir}/bin/nexus.vmoptions
+
+mv ${nexus_tmp_dir} $target_dir
+rm -rf ${tmp_dir}
