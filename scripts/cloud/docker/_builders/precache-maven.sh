fddacc3ca80658808e5a818bf16e0b692b3eecc0
lprimak <lenny@flowlogix.com>
2025-05-06 20:09:40 -0500

    chore(docker): updated JVM options for JDK 24

--- scripts/cloud/docker/_builders/precache-maven.sh
+++ scripts/cloud/docker/_builders/precache-maven.sh
@@ -3,6 +3,6 @@
 echo "Cloning repository"
 git clone -q https://github.com/flowlogix/flowlogix
 echo "Extracting Dependencies"
-JAVA_TOOL_OPTIONS="-XX:UseSVE=0" mvn -B -ntp -q -f flowlogix dependency:go-offline
+JAVA_TOOL_OPTIONS="--sun-misc-unsafe-memory-access=allow" mvn -B -ntp -q -f flowlogix dependency:go-offline
 echo "Packaging repo"
 tar zcf /var/build/repo.tar.gz -C .m2 repository
