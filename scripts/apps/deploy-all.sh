58e9aae2602d0b768b08b7f43b3ae0cfab995118
lprimak <lenny@flowlogix.com>
2025-06-04 18:28:31 -0500

    enh(deploy): added logbook to deployed apps

--- scripts/apps/deploy-all.sh
+++ scripts/apps/deploy-all.sh
@@ -6,3 +6,4 @@ $SCRIPT_DIR/download-all-apps.sh
 
 asadmin deploy --force --contextroot jee-examples --availabilityenabled $HOME/apps/hope-apps/jee-examples.war
 asadmin deploy --force --name apps --availabilityenabled $HOME/apps/hope-apps/hope-apps.ear
+asadmin deploy --force --name logbook --availabilityenabled $HOME/apps/hope-apps/logbook.war
