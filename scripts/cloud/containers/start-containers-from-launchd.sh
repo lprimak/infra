f2a799b6e584a36c601e1196966bc041860ebb48
lprimak <lenny@flowlogix.com>
2025-06-19 21:59:10 -0500

    chore: boot startup for containers

---
+++ scripts/cloud/containers/start-containers-from-launchd.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh -l
+source $HOME/.zshrc
+SCRIPT_DIR=`dirname "$0"`
+
+cd $SCRIPT_DIR; vagrant up
+
+function shutdown() {
+    cd $SCRIPT_DIR; vagrant halt
+    exit 0
+}
+trap shutdown SIGTERM
+while true; do
+    sleep 86400 &
+    wait $!
+done
