c9b55aae3fab48a72e9a0c3c6e005fa46650ec89
lprimak <lenny@flowlogix.com>
2025-07-10 20:22:21 -0500

    chore(maven): remove --sun-misc-unsafe-memory-access=allow option

--- scripts/payara/download-payara.sh
+++ scripts/payara/download-payara.sh
@@ -7,8 +7,6 @@ fi
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-export MAVEN_OPTS="-XX:+IgnoreUnrecognizedVMOptions --sun-misc-unsafe-memory-access=allow"
-
 payara_version=$1
 distribution_package=$2
 payara_basedir=${HOME}/apps/payara
