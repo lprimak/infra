7e6ca7bd9768409883f0429dd3d2771b9b7c72ae
lprimak <lenny@flowlogix.com>
2025-12-04 10:22:13 -0600

    chore(payara): updates to docekr files

--- scripts/payara/download-payara.sh
+++ scripts/payara/download-payara.sh
@@ -49,12 +49,13 @@ fi
 if [ -d $temp_dir/payara7 ]; then
     versioned_dir=payara7
     [ -d "$HOME/Applications/payara" ] && domain_suffix="-p7"
+
+    mv $temp_dir/tyrus* $temp_dir/updates
 fi
 
 mv $temp_dir/post* $temp_dir/sqlite* $temp_dir/jdbc
 mv $temp_dir/asm* $temp_dir/updates
 mv $temp_dir/org.eclipse* $temp_dir/updates
-mv $temp_dir/tyrus* $temp_dir/updates
 
 mv $temp_dir/jdbc/* $temp_dir/${versioned_dir}/glassfish/lib
 modules_dir=$temp_dir/${versioned_dir}/glassfish/modules
