62abbe3c342482e8039ebca0fc6662e2a1e77894
lprimak <lenny@flowlogix.com>
2025-12-18 22:03:39 -0600

    chore: exec haproxy

--- scripts/haproxy/run-haproxy.sh
+++ scripts/haproxy/run-haproxy.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(dirname "$0")
 
 $SCRIPT_DIR/configure-haproxy.sh payara 16380 16443
-$HOME/apps/haproxy/current/bin/haproxy -- $HOME/infra/etc/haproxy/config \
+exec $HOME/apps/haproxy/current/bin/haproxy -- $HOME/infra/etc/haproxy/config \
 $HOME/infra/etc/haproxy/sites/nova-global.cfg $HOME/var/haproxy/frontend.cfg
