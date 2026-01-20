e888ddb6bad6fa00fda0e90672d7dc9f3128c170
lprimak <lenny@flowlogix.com>
2026-01-20 12:31:09 -0600

    enh(local-containers): restore password authentication for vagrant host

--- scripts/cloud/local/install-provisioner.sh
+++ scripts/cloud/local/install-provisioner.sh
@@ -17,3 +17,14 @@ curl https://raw.githubusercontent.com/lprimak/infra/main/scripts/cloud/local/va
 --silent -o /usr/local/bin/vagrant-provision.sh
 chmod +x /usr/local/bin/vagrant-provision.sh
 systemctl enable -q vagrant-provision.service
+
+if [ -f /etc/ssh/sshd_config.d/50-cloud-init.conf ]; then
+  # Update PasswordAuthentication to 'yes'
+  sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/50-cloud-init.conf
+
+  # Restart SSH service to apply changes
+  systemctl restart sshd
+fi
+
+# Change the password for the 'vagrant' user
+echo "vagrant:vagrant" | chpasswd
